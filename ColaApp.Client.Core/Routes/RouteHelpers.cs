a40c4ef053f5e615a9596bd2711f992611529502
cola <1072842511@qq.com>
2020-01-30 17:23:20 +0800

    fff

--- ColaApp.Client.Core/Routes/RouteHelpers.cs
+++ ColaApp.Client.Core/Routes/RouteHelpers.cs
@@ -30,5 +30,28 @@ namespace ColaApp.Core
             // Return combined URL
             return host + relativeUrl;
         }
+
+        public async Task<IActionResult> LoginAsync(string returnUrl)
+        {
+            // Sign out any previous sessions
+            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+
+            // Sign user in with the valid credentials
+            var result = await mSignInManager.PasswordSignInAsync("angelsix", "password", true, false);
+
+            // If successful...
+            if (result.Succeeded)
+            {
+                // If we have no return URL...
+                if (string.IsNullOrEmpty(returnUrl))
+                    // Go to home
+                    return RedirectToAction(nameof(Index));
+
+                // Otherwise, go to the return url
+                return Redirect(returnUrl);
+            }
+
+            return Content("Failed to login", "text/html");
+        }
     }
 }
